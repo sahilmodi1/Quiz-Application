01f1f744182f76020f644dcd42580ef96fddfe7d
sahilmodi1 <60054105+sahilmodi1@users.noreply.github.com>
2023-11-29 23:08:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Quiz from './components/Quiz';
+
+function App() {
+  return (
+    <>
+      <Quiz />
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/Data/QuizData.js
@@ -0,0 +1,42 @@
+export const QuizData = [
+    {
+        question: "Grand Central Terminal, Park Avenue, New York is the world's",
+        options:["largest railway station","highest railway station","longest railway station","None of the above"],
+        answer: 1
+    },
+    {
+        question: "Entomology is the science that studies",
+        options: ["Behavior of human beings","Insects","The origin and history of technical and scientific terms","The formation of rocks"],
+        answer: 2
+    },
+    {
+        question:"Garampani sanctuary is located at",
+        options: ["Junagarh, Gujarat","Diphu, Assam","Kohima, Nagaland","Gangtok, Sikkim"],
+        answer: 2
+    },
+    {
+        question:"For which of the following disciplines is Nobel Prize awarded?",
+        options: ["Physics and Chemistry","Physiology or Medicine","Literature, Peace and Economics","All of the above"],
+        answer: 4
+    },
+    {
+        question: "Hitler party which came into power in 1933 is known as",
+        options: ["Labour Party","Nazi Party","Ku-Klux-Klan","Democratic Party"],
+        answer: 2
+    },
+    {
+        question: "Fastest shorthand writer was",
+        options: ["Dr. G. D. Bist", "J.R.D. Tata", "J.M. Tagore", "Khudada Khan"],
+        answer: 1
+    },
+    {
+        question: "Epsom (England) is the place associated with",
+        options: ["Horse racing","Polo","Shooting","Snooker"],
+        answer: 1
+    },
+    {
+        question: "Golf player Vijay Singh belongs to which country?",
+        options: ["USA","Fiji","India","UK"],
+        answer: 2
+    }
+]
